6fda350391c46920900c4d0c6b0436e01436b7e9
Jai van den Bedem <jaivandenbedem@gmail.com>
2015-07-29 16:07:42 +0300

    Create ADFGX_Encode.java
    Program to encode text using the German WW1 ADFGX cipher (https://en.wikipedia.org/wiki/ADFGVX_cipher)

---
+++ ADFGX_Encode.java
@@ -0,0 +1,527 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import java.util.*; 
+
+public class ADFGX_Encode {
+	public static int[] columns(String inputKeyword, String output, int[] number1) { 
+		// alphabetically order the key word 
+		int count1 = 0; 
+		if (inputKeyword.contains("a") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("a", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("a", track); 
+					count1++; 
+					track = inputKeyword.indexOf("a", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("b") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("b", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("b", track); 
+					count1++; 
+					track = inputKeyword.indexOf("b", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("c") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("c", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("c", track); 
+					count1++; 
+					track = inputKeyword.indexOf("c", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("d") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("d", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("d", track); 
+					count1++; 
+					track = inputKeyword.indexOf("d", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("e") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("e", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("e", track); 
+					count1++; 
+					track = inputKeyword.indexOf("e", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("f") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("f", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("f", track); 
+					count1++; 
+					track = inputKeyword.indexOf("f", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("g") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("g", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("g", track); 
+					count1++; 
+					track = inputKeyword.indexOf("g", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("h") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("h", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("h", track); 
+					count1++; 
+					track = inputKeyword.indexOf("h", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("i") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("i", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("i", track); 
+					count1++; 
+					track = inputKeyword.indexOf("i", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("j") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("j", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("j", track); 
+					count1++; 
+					track = inputKeyword.indexOf("j", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("k") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("k", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("k", track); 
+					count1++; 
+					track = inputKeyword.indexOf("k", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("l") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("l", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("l", track); 
+					count1++; 
+					track = inputKeyword.indexOf("l", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("m") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("m", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("m", track); 
+					count1++; 
+					track = inputKeyword.indexOf("m", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("n") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("n", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("n", track); 
+					count1++; 
+					track = inputKeyword.indexOf("n", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("o") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("o", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("o", track); 
+					count1++; 
+					track = inputKeyword.indexOf("o", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("p") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("p", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("p", track); 
+					count1++; 
+					track = inputKeyword.indexOf("p", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("q") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("q", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("q", track); 
+					count1++; 
+					track = inputKeyword.indexOf("q", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("r") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("r", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("r", track); 
+					count1++; 
+					track = inputKeyword.indexOf("r", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("s") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("s", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("s", track); 
+					count1++; 
+					track = inputKeyword.indexOf("s", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("t") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("t", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("t", track); 
+					count1++; 
+					track = inputKeyword.indexOf("t", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} if (inputKeyword.contains("u") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("u", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("u", track); 
+					count1++; 
+					track = inputKeyword.indexOf("u", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("v") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("v", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("v", track); 
+					count1++; 
+					track = inputKeyword.indexOf("v", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("w") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("w", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("w", track); 
+					count1++; 
+					track = inputKeyword.indexOf("w", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("x") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("x", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("x", track); 
+					count1++; 
+					track = inputKeyword.indexOf("x", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("y") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("y", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("y", track); 
+					count1++; 
+					track = inputKeyword.indexOf("y", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		} 
+		if (inputKeyword.contains("z") == true) { 
+			boolean valid1 = false; 
+			int track = 0; 
+			while (valid1 == false) { 
+				if (inputKeyword.indexOf("z", track) != -1) { 
+					number1[count1] = inputKeyword.indexOf("z", track); 
+					count1++; 
+					track = inputKeyword.indexOf("z", track) + 1; 
+				} else { 
+					valid1 = true; 
+				} 
+			}
+		}
+		return number1;
+	}
+	public static String[][] polybius(String[][] array, String alphabet) { 
+		String input = ""; 
+		boolean	decide = false; 
+		boolean valid = false; 
+		Random randomno = new Random(); 
+		int number = 0; 
+		while (valid == false) { 
+			String enter = JOptionPane.showInputDialog(null,"Please decide if you want to enter your own Polybius square or have a random one generated for you - Press 'p' if you want to enter your own square and 'g' if you want one generated for you"); 
+			if (enter.equalsIgnoreCase("p") == true) { 
+				valid = true; 
+				decide = true; 
+			} else if (enter.equalsIgnoreCase("g") == true) { 
+				valid = true; 
+			} else { 
+				System.out.println("Please enter either 'p' or 'g' ! ");  
+			}
+		} 
+		if (decide == false) { 
+			// generate random square 
+			for (int i = 0; i < 5; i++) { 
+				for (int j = 0; j < 5; j++) { 
+					number = randomno.nextInt(alphabet.length()); 
+					array[i][j] = alphabet.substring(number, number + 1); 
+					alphabet = alphabet.substring(0, number) + alphabet.substring(number + 1, alphabet.length()); 
+				} 
+			}     
+		} else { 
+			// allow user to input their own random square 
+				boolean check = false; 
+				while (check == false) { 
+					input = JOptionPane.showInputDialog(null,"Please enter your array line by line from left to right"); 
+					input = input.toLowerCase(); 
+					if (input.length() == 25 && input.contains("a") == true && input.contains("b") == true && input.contains("c") == true && input.contains("d") == true && input.contains("e") == true && input.contains("f") == true && input.contains("g") == true && input.contains("h") == true && input.contains("i") == true && input.contains("k") == true && input.contains("l") == true && input.contains("m") == true && input.contains("n") == true && input.contains("o") == true && input.contains("p") == true && input.contains("q") == true && input.contains("r") == true && input.contains("s") == true && input.contains("t") == true && input.contains("u") == true && input.contains("v") == true && input.contains("w") == true && input.contains("x") == true && input.contains("y") == true && input.contains("z") == true) { 
+						int x = 0; 
+						for (int i = 0; i < 5; i++) { 
+							for (int y = 0; y < 5; y++) { 
+								array[i][y] = input.substring(x, x + 1); 
+								x++; 
+							}   
+					    } 
+						check = true; 
+					} else { 
+						System.out.println("Please enter the letters together in a block of 25 and omit the letter j ! "); 
+					}
+				} 
+		} 
+		// show square  
+		System.out.println("The following is your unique Polybius Square:"); 
+		System.out.println("  A D F G X");
+		System.out.println("A " + array[0][0] + " " + array[0][1] + " " + array[0][2] + " " + array[0][3] + " " + array[0][4]);
+		System.out.println("D " + array[1][0] + " " + array[1][1] + " " + array[1][2] + " " + array[1][3] + " " + array[1][4]);
+		System.out.println("F " + array[2][0] + " " + array[2][1] + " " + array[2][2] + " " + array[2][3] + " " + array[2][4]);
+		System.out.println("G " + array[3][0] + " " + array[3][1] + " " + array[3][2] + " " + array[3][3] + " " + array[3][4]);
+		System.out.println("X " + array[4][0] + " " + array[4][1] + " " + array[4][2] + " " + array[4][3] + " " + array[4][4]);
+		return array;
+	}
+	public static void main(String[] args) { 
+		String inputKeyword = ""; 
+		boolean valid = false; 
+		// validation mechanism for key word 
+		while (valid == false) { 
+			inputKeyword = JOptionPane.showInputDialog(null,"Please enter your keyword"); 
+			inputKeyword = inputKeyword.trim(); 
+			inputKeyword = inputKeyword.toLowerCase();
+			valid = true;
+			if (inputKeyword.contains(" ") == true) { 
+				valid = false;
+				System.out.println("The key word cannot contain any spaces!");
+			} else if (inputKeyword.matches(".*[0-9].*") == true) {
+				valid = false;
+				System.out.println("The key word cannot contain any numbers!");
+			} else if (inputKeyword.matches("[a-zA-Z0-9 ]*") == false) {
+				valid = false;
+				System.out.println("The key word cannot contain any special characters!");
+			} else if (inputKeyword.length() < 1) { 
+				valid = false;
+			}
+		} 
+		inputKeyword = inputKeyword.toLowerCase(); 
+		System.out.println("Please enter your text"); 
+		// validation mechanism for text 
+		String inputText = ""; 
+		valid = false; 
+		while (valid == false) { 
+			inputText = JOptionPane.showInputDialog(null,"Please enter the text that you wish to encipher");
+			inputText = inputText.trim(); 
+			inputText = inputText.replaceAll(" ", "");
+			inputText = inputText.toLowerCase();
+			valid = true;
+			if (inputText.length() < 1) { 
+				valid = false;
+			} else if (inputText.matches(".*[0-9].*") == true) {
+				valid = false;
+				System.out.println("The key word cannot contain any numbers!");
+			} else if (inputText.matches("[a-zA-Z0-9 ]*") == false) {
+				valid = false;
+				System.out.println("The key word cannot contain any special characters!");
+			}
+		} 
+		inputText = inputText.replace('j', 'i'); 
+		String[][] array = new String[5][5]; 
+		String alphabet = "abcdefghiklmnopqrstuvwxyz"; 
+		// initialise the array 
+		for (int i = 0; i < 5; i++) { 
+			for (int j = 0; j < 5; j++) { 
+				array[i][j] = "ex"; 
+			} 
+		} 
+	    // get the Polybius Square 
+		array = polybius(array, alphabet); 
+		// generate first phase of code using Polybius Square       
+		String output = ""; 
+		int count = 0;  
+		while (count < inputText.length()) { 
+			for (int a = 0; a < 5; a++) { 
+				for (int z = 0; z < 5; z++) { 
+					if (inputText.substring(count, count + 1).equalsIgnoreCase(array[a][z]) == true) { 
+						if (a == 0) { 
+							output = output + "A"; 
+						} else if (a == 1) { 
+							output = output + "D"; 
+						} else if (a == 2) { 
+							output = output + "F"; 
+						} else if (a == 3) { 
+							output = output + "G"; 
+						} else if (a == 4) { 
+							output = output + "X"; 
+						} 
+						if (z == 0) { 
+							output = output + "A"; 
+						} else if (z == 1) { 
+							output = output + "D"; 
+						} else if (z == 2) { 
+							output = output + "F"; 
+						} else if (z == 3) { 
+							output = output + "G"; 
+						} else if (z == 4) { 
+							output = output + "X"; 
+						} 
+					} 
+				} 
+			}   
+			count++; 
+		} 
+		// divide up the output text into columns below each letter of the key word
+		int place = 0; 
+		int dimension = 0;  
+		if (output.length()%inputKeyword.length() != 0) { 
+			dimension = (int)Math.floor((output.length()/inputKeyword.length())) + 1; 
+		} else { 
+			dimension = output.length()/inputKeyword.length();          
+		}      
+		// input the ciphertext into the columns of the key word 
+		String[][] array1 = new String[dimension][inputKeyword.length()]; 
+		int[] number1 = new int[inputKeyword.length()]; 
+		for (int a = 0; a < dimension; a++) { 
+			for (int z = 0; z < inputKeyword.length(); z++) { 
+				if (place > output.length() - 1) { 
+					array1[a][z] = " "; 
+				} else { 
+					array1[a][z] = output.substring(place, place + 1); 
+				} 
+				place++; 
+			}
+		}  
+		number1 = columns(inputKeyword, output, number1);
+		//write out the text down each column from left to right - final step in the enciphering 
+		output = ""; 
+		for (int a = 0; a < inputKeyword.length(); a++) { 
+			for (int z = 0; z < dimension; z++) { 
+				if (array1[z][number1[a]].equals(" ") != true) { 
+					output = output + array1[z][number1[a]]; 
+				} 
+			}   
+				output = output + " "; 
+		} 
+		// print cipher text 
+		JOptionPane.showMessageDialog(null, output);
+	} 
+} 

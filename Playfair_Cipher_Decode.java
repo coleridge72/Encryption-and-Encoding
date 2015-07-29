7077f8597b69739dbd991a7501969b4f852a38b7
Jai van den Bedem <jaivandenbedem@gmail.com>
2015-07-29 14:27:56 +0300

    Update Playfair_Cipher_Decode.java
    This program will decipher text using the Playfair Cipher. Note that the deciphered text will still have "x"'s in it in between double words and at the end if the original parity was odd. These can simply be ignored when reading the message. Also, some "i"'s in the message may actually stand for "j"'s - another by-product of how the Playfair Cipher works!

--- Playfair_Cipher_Decode.java
+++ Playfair_Cipher_Decode.java
@@ -2,117 +2,8 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 
 public class Playfair_Cipher_Decode {
-	public static void main(String[] args) { 
-		String inputKeyword = ""; 
-		boolean valid = false; 
-		// the validation mechanism for the keyword
-		while (valid == false) { 
-			inputKeyword = JOptionPane.showInputDialog(null,"Please enter your keyword"); 
-			inputKeyword = inputKeyword.trim(); 
-			inputKeyword = inputKeyword.toLowerCase();
-			valid = true;
-			// check if there are any spaces 
-			if (inputKeyword.contains(" ") == true) { 
-				valid = false;
-				System.out.println("The key word cannot contain any spaces!");
-			// check if there are any numbers 
-			} else if (inputKeyword.matches(".*[0-9].*") == true) {
-				valid = false;
-				System.out.println("The key word cannot contain any numbers!");
-			// check if there are any special characters 
-			} else if (inputKeyword.matches("[a-zA-Z0-9 ]*") == false) {
-				valid = false;
-				System.out.println("The key word cannot contain any special characters!");
-			// check if a keyword has been entered 
-			} else if (inputKeyword.length() < 1 == true) {
-				valid = false;
-			}
-		} 
-		inputKeyword = inputKeyword.toLowerCase(); 
-		String inputText = "";
-		valid = false;
-		// the validation method for the text
-		while (valid == false) { 
-			inputText = JOptionPane.showInputDialog(null,"Please enter the text that you wish to decipher");
-			inputText = inputText.trim(); 
-			inputText = inputText.replaceAll(" ", "");
-			valid = true;
-			// check that text has been entered 
-			if (inputText.length() < 1) { 
-				valid = false;
-			// check that there are no numbers 
-			} else if (inputText.matches(".*[0-9].*") == true) {
-				valid = false;
-				System.out.println("The text cannot contain any numbers!");
-			// check that there are no special characters 
-			} else if (inputText.matches("[a-zA-Z0-9 ]*") == false) {
-				valid = false;
-				System.out.println("The text cannot contain any special characters!");
-			}
-		} 
-		// create the letter square in the form of a 5x5 array 
-		String[][] array = new String[5][5]; 
-		// initialise the array - ex is just an initial value string 
-		for (int i = 0; i < 5; i++) { 
-			for (int j = 0; j < 5; j++) { 
-				array[i][j] = "ex";   
-			} 
-		}		
-		String string1 = ""; 
-		String string2 = ""; 
-		boolean accept = false;                           
-		int count = 1; 
-		// remove repeat letter's in key word 
-		if (inputKeyword.substring(0, 1).equalsIgnoreCase("j") == true) { 
-			inputKeyword = inputKeyword.substring(1, inputKeyword.length()); 
-		} 
-		// remove the letter j from the keyword (Playfair ciphers are 5x5 = 25 letters, so i and j are used interchangeably) 
-		while (accept == false) { 
-				if (inputKeyword.indexOf(inputKeyword.substring(count, count + 1)) != inputKeyword.lastIndexOf(inputKeyword.substring(count, count + 1)) || inputKeyword.substring(count, count + 1).equalsIgnoreCase("j") == true) { 
-					string1 = inputKeyword.substring(0, inputKeyword.lastIndexOf(inputKeyword.substring(count, count + 1))); 
-					string2 = inputKeyword.substring(inputKeyword.lastIndexOf(inputKeyword.substring(count, count + 1)) + 1, inputKeyword.length()); 
-					inputKeyword = string1.concat(string2); 
-				} else { 
-					count++; 
-				} 
-				if (count == inputKeyword.length()) { 
-					accept = true; 
-				} 				
-		} 
-		int count1 = 0; 
-		// fill the array with the key word 
-		for (int i = 0; i < 5 && count1 < inputKeyword.length(); i++) { 
-			for (int j = 0; j < 5 && count1 < inputKeyword.length(); j++) { 
-				array[i][j] = inputKeyword.substring(count1, count1 + 1); 
-				count1++;  				
-			} 
-		} 
-		// eliminate character's which are not required 
-		String list = "abcdefghiklmnopqrstuvwxyz "; 
-		for (int a = 0; a < inputKeyword.length(); a++) { 
-			for (int q = 0; q < list.length(); q++) {   
-				if (inputKeyword.substring(a, a + 1).equalsIgnoreCase(list.substring(q, q + 1)) == true) { 
-					string1 = list.substring(0, q); 
-					string2 = list.substring(q + 1, list.length()); 
-					list = string1.concat(string2); 
-				} 
-			} 
-		} 
-		int count2 = 0; 
-		// fill rest of the array 
-		for (int i = 0; i < 5; i++) { 
-			for (int j = 0; j < 5; j++) { 	
-				if (array[i][j].equalsIgnoreCase("ex") == true) { 
-					array[i][j] = list.substring(count2, count2 + 1); 
-					count2++; 
-				} 
-			} 
-		} 
-
-		string1 = " "; 
-		inputText = string1.concat(inputText); 
-		inputText = inputText + " "; 
-		String output = ""; 
+	public static String decipher(String inputText, String[][] array) { 
+		String output = "";
 		int position11 = 0; 
 		int position12 = 0;         
 		int position21 = 0; 
@@ -228,6 +119,120 @@ public class Playfair_Cipher_Decode {
 				output = output + " "; 
 			} 
 		} 
+		return output;
+	}
+	public static void main(String[] args) { 
+		String inputKeyword = ""; 
+		boolean valid = false; 
+		// the validation mechanism for the keyword
+		while (valid == false) { 
+			inputKeyword = JOptionPane.showInputDialog(null,"Please enter your keyword"); 
+			inputKeyword = inputKeyword.trim(); 
+			inputKeyword = inputKeyword.toLowerCase();
+			valid = true;
+			// check if there are any spaces 
+			if (inputKeyword.contains(" ") == true) { 
+				valid = false;
+				System.out.println("The key word cannot contain any spaces!");
+			// check if there are any numbers 
+			} else if (inputKeyword.matches(".*[0-9].*") == true) {
+				valid = false;
+				System.out.println("The key word cannot contain any numbers!");
+			// check if there are any special characters 
+			} else if (inputKeyword.matches("[a-zA-Z0-9 ]*") == false) {
+				valid = false;
+				System.out.println("The key word cannot contain any special characters!");
+			// check if a keyword has been entered 
+			} else if (inputKeyword.length() < 1 == true) {
+				valid = false;
+			}
+		} 
+		inputKeyword = inputKeyword.toLowerCase(); 
+		String inputText = "";
+		valid = false;
+		// the validation method for the text
+		while (valid == false) { 
+			inputText = JOptionPane.showInputDialog(null,"Please enter the text that you wish to decipher");
+			inputText = inputText.trim(); 
+			inputText = inputText.replaceAll(" ", "");
+			valid = true;
+			// check that text has been entered 
+			if (inputText.length() < 1) { 
+				valid = false;
+			// check that there are no numbers 
+			} else if (inputText.matches(".*[0-9].*") == true) {
+				valid = false;
+				System.out.println("The text cannot contain any numbers!");
+			// check that there are no special characters 
+			} else if (inputText.matches("[a-zA-Z0-9 ]*") == false) {
+				valid = false;
+				System.out.println("The text cannot contain any special characters!");
+			}
+		} 
+		// create the letter square in the form of a 5x5 array 
+		String[][] array = new String[5][5]; 
+		// initialise the array - ex is just an initial value string 
+		for (int i = 0; i < 5; i++) { 
+			for (int j = 0; j < 5; j++) { 
+				array[i][j] = "ex";   
+			} 
+		}		
+		String string1 = ""; 
+		String string2 = ""; 
+		boolean accept = false;                           
+		int count = 1; 
+		// remove repeat letter's in key word 
+		if (inputKeyword.substring(0, 1).equalsIgnoreCase("j") == true) { 
+			inputKeyword = inputKeyword.substring(1, inputKeyword.length()); 
+		} 
+		// remove the letter j from the keyword (Playfair ciphers are 5x5 = 25 letters, so i and j are used interchangeably) 
+		while (accept == false) { 
+				if (inputKeyword.indexOf(inputKeyword.substring(count, count + 1)) != inputKeyword.lastIndexOf(inputKeyword.substring(count, count + 1)) || inputKeyword.substring(count, count + 1).equalsIgnoreCase("j") == true) { 
+					string1 = inputKeyword.substring(0, inputKeyword.lastIndexOf(inputKeyword.substring(count, count + 1))); 
+					string2 = inputKeyword.substring(inputKeyword.lastIndexOf(inputKeyword.substring(count, count + 1)) + 1, inputKeyword.length()); 
+					inputKeyword = string1.concat(string2); 
+				} else { 
+					count++; 
+				} 
+				if (count == inputKeyword.length()) { 
+					accept = true; 
+				} 				
+		} 
+		int count1 = 0; 
+		// fill the array with the key word 
+		for (int i = 0; i < 5 && count1 < inputKeyword.length(); i++) { 
+			for (int j = 0; j < 5 && count1 < inputKeyword.length(); j++) { 
+				array[i][j] = inputKeyword.substring(count1, count1 + 1); 
+				count1++;  				
+			} 
+		} 
+		// eliminate character's which are not required 
+		String list = "abcdefghiklmnopqrstuvwxyz "; 
+		for (int a = 0; a < inputKeyword.length(); a++) { 
+			for (int q = 0; q < list.length(); q++) {   
+				if (inputKeyword.substring(a, a + 1).equalsIgnoreCase(list.substring(q, q + 1)) == true) { 
+					string1 = list.substring(0, q); 
+					string2 = list.substring(q + 1, list.length()); 
+					list = string1.concat(string2); 
+				} 
+			} 
+		} 
+		int count2 = 0; 
+		// fill rest of the array 
+		for (int i = 0; i < 5; i++) { 
+			for (int j = 0; j < 5; j++) { 	
+				if (array[i][j].equalsIgnoreCase("ex") == true) { 
+					array[i][j] = list.substring(count2, count2 + 1); 
+					count2++; 
+				} 
+			} 
+		} 
+
+		string1 = " "; 
+		inputText = string1.concat(inputText); 
+		inputText = inputText + " "; 
+		String output = ""; 
+		output = decipher(inputText, array);
 		JOptionPane.showMessageDialog(null, output);
 	} 
 }
